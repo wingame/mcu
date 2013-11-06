379c6513a223915d707bb99e7762bddf461b07b9
wingame <liang.spark@gmail.com>
2013-11-06 16:51:34 +0800

    env_case.asm snapshot

--- 8051/env_case.asm
+++ 8051/env_case.asm
@@ -1,3 +1,7 @@
+; 生物安全柜需求
+; 考虑到时间存储的不是很合理，考虑修改成为以秒为单位的存储方式
+; 有2种，1 设备启动的时间，2 系统时钟，用于任务的运行安排
+; 在进行修改之前做一个提交保存进度
 
 ; 6 p1.7 fan1
 ; 7 fan_out
@@ -50,13 +54,34 @@ rb3r6	equ	1eh
 rb3r7	equ	1fh
 
 
+; screen command code
+SCHL_HALT		equ 9
+SETTING			equ 5
+KEY_OK			equ 21
+KEY_C			equ 20
+KEY_0			equ 10
+KEY_1			equ 11
+KEY_2			equ 12
+KEY_3			equ 13
+KEY_4			equ 14
+KEY_5			equ 15
+KEY_6			equ 16
+KEY_7			equ 17
+KEY_8			equ 18
+KEY_9			equ 19
+START_C			equ 30
+END_C			equ 31
+;green_led	bit p1.3
+;red_led		bit	p1.4
 
-OPEN_DIALOG		equ 9
-CLOSE_DIALOG	equ 21
+H1				equ p1.1
+H2				equ p1.0
+BEEP			equ	p0.7
+FANO2			equ p1.4
+FANO1			equ p1.6
 
-
-green_led	bit p1.3
-red_led		bit	p1.4
+CUR_START_X		equ 380
+CUR_START_Y		equ 160
 
 
 temp1		equ	30h
@@ -75,7 +100,7 @@ temp3		equ	32h
 ; 8 3c
 buf_first		equ	33h
 buf_last		equ	34h
-buf_empty		bit	20h.0
+;@buf_empty		bit	20h.0
 
 
 xl			equ	35h
@@ -83,33 +108,61 @@ xh			equ	36h
 yl			equ	37h
 yh			equ	38h
 
-hour		equ	39h
-minute		equ	3ah
-second		equ	3bh
+; 设备启动时间
+run_hour	equ	39h
+run_minute		equ	3ah
+run_second		equ	3bh
 ; 2 byte
 day			equ	3ch		;3dh
+; 内部时钟的计数器，每次算到8就是1秒，这个设定误差巨大。需要改进
 tick		equ	3eh
 
-send_cmd_idx	equ	3fh
+run_timing_idx	equ	3fh
 ; 当second有更新，标志设1，然后计算一次minute和hour
-clock_tick	bit	20h.1
-show_sys_clock bit 20h.3
+;@clock_tick	bit	20h.1
 
 
 
 recv_cmd_idx	equ 40h
 recv_cmd		equ 41h
+;@recv_cmd_ready	bit	20h.2
+
+
+; 系统时钟
+tm_hour			equ 42h
+tm_minute		equ 43h
+tm_second		equ 44h
+
+; task start timing
+task_start_h	equ 45h
+task_start_m	equ 46h
+task_start_s	equ 47h
+
+; task end timing
+task_end_h		equ 48h
+task_end_m		equ 49h
+task_end_s		equ 4ah
+
+;输入框的光标位置,0~11, 0~5 开始时间， 6~11 结束时间
+cursor_idx		equ 4bh
+
+; bit definition
+buf_empty		bit	20h.0
+clock_tick		bit	20h.1
 recv_cmd_ready	bit	20h.2
 
+flag_func		equ 21h
+func_halt_b		bit flag_func.0
+func_setting_b	bit flag_func.1
 
 
+
+; uart buffer definition
 PIPO_TOP		equ 0d0h
 PIPO_BUTTON		equ	0c0h
 
 
 
-LED_MDIN	equ	P1MDIN
-LED_MDOUT	equ	P1MDOUT
 
 
 	cseg	at 0
@@ -124,61 +177,102 @@ org 7bh
 t0overflow_int:
 	inc		tick
 	mov		a,tick
+	cjne	a,#7,t0ovf_1
+; 重新设置tl时需要先停止时钟，根据336的PDF 182页的文档,倒数第二段
+; 8300 9小时快了30秒
+; 8200 11小时快了15秒
+	clr		tr0
+	mov		tl0,#low(8000)
+	mov		th0,#high(8000)
+	setb	tr0
+	sjmp	t0ovf_exit
+t0ovf_1:
 	cjne	a,#8,t0ovf_exit
 	mov		tick,#0
 	setb	clock_tick
-	inc		second
+	inc		run_second
+	inc		tm_second
 t0ovf_exit:
 	pop		acc
 	reti
 
+func_proc:
 
+
+	mov		a,recv_cmd
+	clr		c
+	subb	a,#20
+	jnc		next1
+	mov		a,recv_cmd
+	clr		c
+	subb	a,#10
+	mov		r0,cursor_idx
+	movx	@r0,a
+	jc		next1
+; time input
+	lcall	show_digital
+
+; work here
+
+	mov		a,cursor_idx
+	clr		c
+	subb	a,#12
+	jz		next1
+	inc		cursor_idx
+; time input end
+next1:
+	ret
+func_ok:
+	clr		func_halt_b
+	clr		func_setting_b
+	lcall	close_cursor
+	ret
+func_c:
+	clr		func_halt_b
+	clr		func_setting_b
+	lcall	close_cursor
+	ret
 
 start:
 	clr		ea
 	mov		sp,#0cfh
 	mov		psw,#0
 
-
-;	mov		cmd_stage,#0	;idle
+; debug ----------------------------------------------
+	mov		tm_hour,#20
+	mov		tm_minute,#13
+	mov		tm_second,#40
+; debug end ------------------------------------------
 	
-;clr		debug_stop	
 
 	lcall	init
 	lcall	init_timer_01
-	lcall	init_led
+	lcall	init_port
 	lcall	init_uart_02
 	lcall	init_final
-	clr		red_led
 	lcall	long_long_delay
-	clr		green_led
+
 	clr		a
 	mov		day,a
 	mov		day+1,a
-	mov		hour,a
-	mov		second,a
-	mov		minute,a
+	mov		run_hour,a
+	mov		run_second,a
+	mov		run_minute,a
 	mov		tick,a
 	
 	mov		recv_cmd_idx,a
-	setb	show_sys_clock
 	
+	mov		flag_func,#0
+;	setb	show_sys_clock
+	
+;	setb	H1
+;	setb	H2
+;	setb	BEEP
+;	setb	FANO2
 
 	
 
-;	lcall	long_delay
-;	lcall	tft_cls
 
-;	mov		xh,#0
-;	mov		yh,#high(300h)
-;	mov		xl,#10
-;	mov		yl,#low(300h)
-;	mov		dptr,#string
-;	lcall	puts32
-;	lcall	long_delay
-
-	
-	
 	lcall	long_delay
 
 ;	lcall	read_uart
@@ -188,11 +282,6 @@ start:
 	
 	lcall	long_long_delay
 	lcall	long_long_delay
-; set background color
-;	mov		r0,#0
-;	mov		r1,#4
-;	mov		r2,#40h
-;	lcall	send_cmd
 	mov		a,#0aah
 	lcall	send_uart
 	mov		a,#42h
@@ -218,40 +307,116 @@ start:
 
 
 	setb	ea
-;	mov		xl,#40
-;	mov		xh,#0
-;	mov		yl,#low(380)
-;	mov		yh,#high(380)
-;	mov		r2,#59h
-;	lcall 	vputs32_h
 loop:
 	lcall	read_uart
 	jb		buf_empty,check_clock
-	cpl		red_led
 	lcall	parse_cmd
 	jnb		recv_cmd_ready,check_clock
-;	setb	green_led
 	clr		recv_cmd_ready
+
 	mov		a,recv_cmd
-	cjne	a,#OPEN_DIALOG,loop1
-	clr		show_sys_clock
+	cjne	a,#SCHL_HALT,loop1
+	ljmp	func_halt_start
 loop1:
-	cjne	a,#CLOSE_DIALOG,check_clock
-	setb	show_sys_clock
+	cjne	a,#SETTING,task_proc
+	ljmp	func_setting_start
+task_proc:
+	lcall	func_proc
+	mov		a,recv_cmd
+	cjne	a,#KEY_OK,loop2
+	lcall	func_ok
+loop2:
+	cjne	a,#KEY_C,check_clock
+	lcall	func_c
 check_clock:
 	jnb		clock_tick,loop
 	clr		clock_tick
 
-	lcall	update_clock
+	lcall	update_run_clock
+	lcall	update_tm_clock
 ;	cpl		green_led
 	sjmp	loop
+
+func_halt_start:
+	setb	func_halt_b
+	mov		cursor_idx,#0
+	ljmp	task_proc
+func_setting_start:
+	setb	func_setting_b
+	mov		cursor_idx,#0
+	mov		xl,#low(CUR_START_X)
+	mov		xh,#high(CUR_START_X)
+	mov		yl,#low(CUR_START_Y)
+	mov		yh,#high(CUR_START_Y)
+	call	set_cursor
+;sjmp	$
+	ljmp	task_proc
+close_cursor:
+	clr		a
+	mov		dph,a
+	mov		dpl,a
+	mov		r0,a
+	mov		a,#10
+	movx	@r0,a
+	inc		r0
+	mov		a,#44h	; 光标显示
+	movx	@r0,a
+	inc		r0
+	mov		a,#00h	; cursor_en
+	movx	@r0,a
+	lcall	tft_cmd
+	ret
+	
+set_cursor:
+	clr		a
+	mov		dph,a
+	mov		dpl,a
+	mov		r0,a
+	mov		a,#10
+	movx	@r0,a
+	inc		r0
+	mov		a,#44h	; 光标显示
+	movx	@r0,a
+	inc		r0
+	mov		a,#01h	; cursor_en
+	movx	@r0,a
+	inc		r0
+
+	mov		a,xh	; xh
+	movx	@r0,a
+	inc		r0
+	mov		a,xl	; xl
+	movx	@r0,a
+	inc		r0
+	mov		a,yh	; yh
+	movx	@r0,a
+	inc		r0
+	mov		a,yl	; yl
+	movx	@r0,a
+
+	inc		r0
+	mov		a,#0fh	; cursor width
+	movx	@r0,a
+	inc		r0
+	mov		a,#0fh	; cursor hight
+	movx	@r0,a
+	inc		r0
+	mov		a,#01h	; cursor_blink_en
+	movx	@r0,a
+
+	inc		r0
+	mov		a,#80h	; blink time
+	movx	@r0,a
+	lcall	tft_cmd
+	ret
+
 day_conv:
 	mov		a,day
 	clr		c
 	subb	a,#low(10000)
 	mov		a,day+1
 	subb	a,#high(10000)
-	mov		r0,send_cmd_idx
+	mov		r0,run_timing_idx
 
 	jc		day_conv_lbl1
 	mov		a,#'9'
@@ -334,26 +499,95 @@ dc_lbl10:
 	movx	@r0,a
 day_conv_ret:
 	inc		r0
-	mov		send_cmd_idx,r0
+	mov		run_timing_idx,r0
 	ret
-update_clock:
-	mov		a,second
+show_digital:
+; 从输入读取的按键值转换成ASCII码保持着xram中12位移地址，以0结尾的字符串方式存放
+; 然后显示出来。
+	mov		r0,#12
+	add		a,#30h
+	movx	@r0,a
+	inc		r0
+	clr		a
+	movx	@r0,a
+	mov		r0,#12
+	mov		xl,#low(CUR_START_X)
+	mov		xh,#high(CUR_START_X)
+	mov		yl,#low(CUR_START_Y)
+	mov		yh,#high(CUR_START_Y)
+	mov		a,cursor_idx
+	mov		b,#13
+	mul		ab
+	clr		c
+	addc	a,xl
+	mov		xl,a
+	mov		a,b
+	addc	a,xh
+	mov		xh,a
+	lcall	iputs24
+	
+	ret
+update_tm_clock:
+	mov		a,tm_second
+	cjne	a,#60,uc_show_tm_clock
+	mov		tm_second,#0
+	inc		tm_minute
+	mov		a,tm_minute
+	cjne	a,#60,uc_show_tm_clock
+	mov		tm_minute,#0
+	inc		tm_hour
+	mov		a,tm_hour
+	cjne	a,#24,uc_show_tm_clock
+	mov		tm_hour,#0
+uc_show_tm_clock:
+	mov		a,flag_func
+	jz		uc_show_tm_clock_1
+	ret
+uc_show_tm_clock_1:
+	mov		r0,#0
+	mov		r2,tm_hour
+	acall	hex_bcdascii
+	inc		r0
+	mov		a,#':'
+	movx	@r0,a
+	inc		r0
+	mov		r2,tm_minute
+	acall	hex_bcdascii
+	inc		r0
+	mov		a,#':'
+	movx	@r0,a
+	inc		r0
+	mov		r2,tm_second
+	acall	hex_bcdascii
+	inc		r0
+	clr		a
+	movx	@r0,a
+	mov		xl,#low(350)
+	mov		xh,#high(350)
+	mov		yl,#low(440)
+	mov		yh,#high(440)
+	mov		r0,#0
+	lcall	iputs24
+	ret
+update_run_clock:
+	mov		a,run_second
 	cjne	a,#60,uc_show_clock
-	mov		second,#0
-	inc		minute
-	mov		a,minute
+	mov		run_second,#0
+	inc		run_minute
+	mov		a,run_minute
 	cjne	a,#60,uc_show_clock
-	mov		minute,#0
-	inc		hour
-	mov		a,hour
+	mov		run_minute,#0
+	inc		run_hour
+	mov		a,run_hour
 	cjne	a,#24,uc_show_clock
-	mov		hour,#0
+	mov		run_hour,#0
 	inc		day
 	mov		a,day
 	jnz		uc_show_clock
 	inc		day+1
 uc_show_clock:
-	jb		show_sys_clock,uc_show_clock_1
+	mov		a,flag_func
+	jz		uc_show_clock_1
 	ret
 uc_show_clock_1:
 	mov		r0,#0
@@ -364,9 +598,9 @@ uc_sc_1:
 	movx	@r0,a
 	inc		r0
 	cjne	r0,#10,uc_sc_1
-	mov		send_cmd_idx,r0
+	mov		run_timing_idx,r0
 	
-	
+	; 判断日期是否为0
 	mov		a,day+1
 	jz		skip_1
 	sjmp	skip_2
@@ -381,35 +615,36 @@ skip_2:
 	mov		a,#0ech
 	movx	@r0,a
 	inc		r0
-	mov		send_cmd_idx,r0
+	mov		run_timing_idx,r0
 skip_3:
 	
-	mov		r0,send_cmd_idx
-	mov		r2,hour
+	mov		r0,run_timing_idx
+	mov		r2,run_hour
 	acall	hex_bcdascii
 	inc		r0
 	mov		a,#':'
 	movx	@r0,a
 	inc		r0
-	mov		r2,minute
+	mov		r2,run_minute
 	acall	hex_bcdascii
 	inc		r0
 	mov		a,#':'
 	movx	@r0,a
 	inc		r0
-	mov		r2,second
+	mov		r2,run_second
 	acall	hex_bcdascii
 	inc		r0
 	clr		a
 	movx	@r0,a
 
-	mov		xl,#40
+	mov		xl,#2
 	mov		xh,#0
-	mov		yl,#low(380)
-	mov		yh,#high(380)
+	mov		yl,#low(440)
+	mov		yh,#high(440)
 	mov		r0,#0
-	lcall	iputs32
+	lcall	iputs24
 	ret
+
 ;[in] r2
 ;[out] @r0
 hex_bcdascii:
@@ -434,6 +669,7 @@ hbcd_exit:
 	inc		r0
 	movx	@r0,a
 	ret
+; iputs[xx] = 32 24 16, 在指定位置显示以0结束的字符串 字符串开始位置由r0指定
 iputs32:
 	mov		r2,#55h
 	sjmp	iputs
@@ -473,9 +709,30 @@ iputs_exit:
 	lcall	send_uart
 	mov		a,#3ch
 	lcall	send_uart
+ret_label:
 	ret
+tft_cmd:
+	movx	a,@dptr
+	jz		ret_label
+	inc		dptr
+	mov		r1,a			; amount
+	mov		temp1,#0		; index
+	mov		a,#0aah
+	lcall	send_uart
+tft_cmd_1:
+	mov		a,r1
+	cjne	a,temp1,tft_cmd_2
+	sjmp	iputs_exit
+tft_cmd_2:
+	movx	a,@dptr
+	inc		temp1
+	inc		dptr
+	lcall	send_uart
+	sjmp	tft_cmd_1
+;ret
 
-; receive command function **********************************************************************
+;receive command function **********************************************************************
+; 只处理按键抬起时的事件。
 parse_cmd:
 	mov		r2,a
 	mov		a,recv_cmd_idx
@@ -491,6 +748,7 @@ cmd_operate:
 	sjmp	byte6
 	sjmp	byte7
 ;	sjmp	byte8
+;byte8:
 	cjne	r2,#3ch,bad_cmd
 	setb	recv_cmd_ready
 	mov		recv_cmd_idx,#0
@@ -498,15 +756,15 @@ cmd_operate:
 byte1:
 
 	cjne	r2,#0aah,bad_cmd
-;	setb	green_led
+	sjmp	byte3
 byte2:
+	cjne	r2,#078h,bad_cmd		; 取消79h处理（按钮按下的操作）,只处理离开的操作
 byte3:
-	inc		recv_cmd_idx
+	inc		recv_cmd_idx			; 假设按键的定义是0~255,高位永远是0
 	ret
 byte4:
 	mov		recv_cmd,r2
-	inc		recv_cmd_idx
-	ret
+	sjmp	byte3
 byte5:
 	cjne	r2,#0cch,bad_cmd
 	sjmp	byte3
@@ -516,9 +774,6 @@ byte6:
 byte7:
 	cjne	r2,#0c3h,bad_cmd
 	sjmp	byte3
-	
-
-	ret
 bad_cmd:
 	mov		recv_cmd_idx,#0
 	clr		recv_cmd_ready
@@ -531,13 +786,15 @@ read_uart:
 	clr		ea
 	mov		a,buf_first
 	cjne	a,buf_last,ru_1
-	setb	ea
 	setb	buf_empty
+	setb	ea
+; atom operate end
 ;	setb	red_led
 	ret
 ru_1:
-	setb	ea
 	clr		buf_empty
+; atom operate end
+	setb	ea
 	mov		r0,a
 	inc		a
 
@@ -636,7 +893,6 @@ send_uart:
 	ret
 
 ; uart function end *********************************************************************
-
 init:
 	; set Internal oscillator
 	; OSCICN [IOSCEN|IFRDY|SUSPEND|STSYNC|-|-|IFCN1|IFCN0]
@@ -666,26 +922,44 @@ init:
 ;	djnz	r0,init_1
 	ret
 init_final:
+	mov		a,#0
+	mov		P0,a
+	mov		P1,a
+	
 	mov		XBR0,#1
 	mov		XBR1,#40h			; enable Crossbar and weak pull-ups
 	ret
-init_led:
-	mov		a,LED_MDIN
-	setb	acc.3
-	setb	acc.4
-	mov		LED_MDIN,a
-	mov		a,LED_MDOUT
-	setb	acc.3
-	setb	acc.4
-	mov		LED_MDOUT,a
+init_port:
+	mov		a,P1MDIN
+;	setb	acc.0
+;	setb	acc.1
+;	setb	acc.4
+	orl		a,#00010011b
+	mov		P1MDIN,a
+
+	mov		a,P1MDOUT
+	orl		a,#00010011b
+;	setb	acc.0
+;	setb	acc.1
+;	setb	acc.4
+	mov		P1MDOUT,a
+	
+	mov		a,P0MDIN
+	setb	acc.7
+	mov		P0MDIN,a
+	mov		a,P0MDOUT
+	setb	acc.7
+	mov		P0MDOUT,a
 	ret
 init_timer_01:
+;16位的计数器溢出7次然后额外数 10923 次为 1秒(1024*1024)
+; 如果按照1000×1000算就是 51665
 	; set timer0 to mode 1 16bit
 	; C/T0 =0
 	; GATE0=0
 	mov		tmod,#1
 	mov		tcon,#0
-	;timer0 us /48 prescaler
+	;timer0 use /48 prescaler
 	mov		ckcon,#2
 
 	; enable timer0 overflow flag
